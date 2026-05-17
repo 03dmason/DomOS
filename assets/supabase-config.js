a0ed52d3c7fd5fd6c7d9c5128127f0ce28383f3c
03dmason <dom.mason@btopenworld.com>
2026-05-17 01:59:10 +0100

    Update supabase-config.js

--- assets/supabase-config.js
+++ assets/supabase-config.js
@@ -1,4 +1,6 @@
+// DomOS Supabase configuration
+
 window.DOMOS_SUPABASE = {
-  url: https://tthgpohmylntizgebvip.supabase.co,
-  anonKey: sb_publishable_X4rUGbutTocAvcjQ_PWNHA_PsTek6_2
+  url: "https://tthgpohmylntizgebvip.supabase.co",
+  anonKey: "sb_publishable_X4rUGbutTocAvcjQ_PWNHA_PsTek6_2"
 };
